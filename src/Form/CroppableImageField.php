4e1260a903593b0cf144f5db69ca836b2267ec9f
Leo Chen <leo@leochen.co.nz>
2021-08-07 15:50:27 +1200

    added ratio calculation

--- src/Form/CroppableImageField.php
+++ src/Form/CroppableImageField.php
@@ -137,9 +137,25 @@ class CroppableImageField extends CompositeField
 
         if (isset($this->fields['CropperRatio'])) {
             $this->fields['CropperRatio']->setValue($ratio);
+            $this->setRightTitle('Cropper ratio: ' . $this->float2rat($ratio));
         }
 
         return $this;
+    }
+
+    private function float2rat($n, $tolerance = 1.e-6) {
+        $h1=1; $h2=0;
+        $k1=0; $k2=1;
+        $b = 1/$n;
+        do {
+            $b = 1/$b;
+            $a = floor($b);
+            $aux = $h1; $h1 = $a*$h1+$h2; $h2 = $aux;
+            $aux = $k1; $k1 = $a*$k1+$k2; $k2 = $aux;
+            $b = $b-$a;
+        } while (abs($n-$h1/$k1) > $n*$tolerance);
+
+        return "$h1:$k1";
     }
 
     public function setFolderName($folderName)
